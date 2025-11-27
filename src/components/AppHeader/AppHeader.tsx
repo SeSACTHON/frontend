5f393f12191e524ab8a6823419d8bb66f31594c2
suji8073 <suzy8073@naver.com>
2025-11-27 23:58:19 +0900

    [feat] AppHeader 내 rightIcon 추가
    - 분석 결과 페이지의 AppHeader에서 home 화면으로 이동을 위한 아이콘 필요
    - pathname을 이용해 /camera/answer 시 해당 아이콘 보여주도록 분기 처리

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,5 +1,6 @@
 import { useLocation, useNavigate } from 'react-router-dom';
 import GoBack from '@/assets/icons/go_back.svg';
+import Cancel from '@/assets/icons/iconoir_cancel.svg';
 
 const getTitle = (pathname: string) => {
   switch (pathname) {
@@ -19,7 +20,7 @@ const AppHeader = () => {
   const { pathname } = useLocation();
 
   return (
-    <div className='max-w-app h-app-header absolute top-0 flex w-full items-center pl-7.5'>
+    <div className='max-w-app h-app-header absolute top-0 flex w-full items-center justify-between px-7.5'>
       <img
         src={GoBack}
         alt='GoBack'
@@ -29,6 +30,15 @@ const AppHeader = () => {
       <span className='text-text-primary absolute left-1/2 -translate-x-1/2 text-[18px] leading-8 font-bold tracking-[0.07px]'>
         {getTitle(pathname)}
       </span>
+
+      {pathname === '/camera/answer' && (
+        <img
+          src={Cancel}
+          alt='Cancel'
+          onClick={() => navigate('/home')}
+          className='h-7.5 w-7.5 cursor-pointer'
+        />
+      )}
     </div>
   );
 };
